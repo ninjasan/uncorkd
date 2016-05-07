801a288c7d915ff9c5d3685d7ff7f8a2e9611880
pooja <pmathur2@gmail.com>
2016-05-07 09:07:06 -0700

    feat: Winery Check-in
    You can check-in to a winery now and see the last time you visited that
    winery.
    
    And by "you" I mean everyone, since I haven't built login functionality
    yet. Everything is user_id = 1 at this point.

--- uncorkd/main/controllers.py
+++ uncorkd/main/controllers.py
@@ -1,7 +1,7 @@
 __author__ = 'poojm'
 
 from flask import Blueprint, render_template, url_for, redirect, request
-
+from datetime import date, time
 from uncorkd.data.models import Winery, Wine, User, Visit, Tasting, Purchase
 from uncorkd.data.dbsession import session
 
@@ -67,7 +67,7 @@ def add_winery():
                                website=request.form['website'])
         session.add(winery_to_add)
         session.commit()
-        '''TODO: flash message'''
+        #TODO: flash message
         return redirect(url_for('.get_wineries'))
     else:
         return render_template("add_winery.html")
@@ -76,8 +76,31 @@ def add_winery():
 @main.route('wineries/<int:winery_id>/')
 @main.route('wineries/<int:winery_id>/wines/')
 def get_wines(winery_id):
+    winery = session.query(Winery).filter(Winery.id == winery_id).one()
     wines = session.query(Wine).filter(Wine.winery_id == winery_id).all()
-    return render_template("wines.html", wines=wines)
+    #todo: get signed in user
+    user_Id = 1
+    last_visit = session.query(Visit).\
+                    filter(Visit.user_id == user_Id,
+                           Visit.winery_id == winery_id).\
+                    order_by(Visit.date.desc()).first()
+    print(last_visit)
+    return render_template("wines.html", winery=winery,
+                                         wines=wines,
+                                         last_visit=last_visit)
+
+
+@main.route('wineries/<int:winery_id>/checkin/', methods=['POST'])
+def winery_visit(winery_id):
+    if request.method == 'POST':
+        #todo: change userId to actual userId
+        check_in = Visit(user_id=1,
+                         winery_id=winery_id,
+                         date=date.today())
+        session.add(check_in)
+        session.commit()
+        #todo: flash message
+        return redirect(url_for('.get_wines', winery_id=winery_id))
 
 
 @main.route('wineries/<int:winery_id>/wines/<int:wine_id>/')
